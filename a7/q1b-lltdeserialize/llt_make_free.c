cbc75288066af24c04e57296ab8c6c62039ab714
Nomair Naeem <nanaeem@uwaterloo.ca>
2025-03-20 19:41:09 -0400

    Added integrity statements to A7

--- a7/q1b-lltdeserialize/llt_make_free.c
+++ a7/q1b-lltdeserialize/llt_make_free.c
@@ -1,3 +1,18 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+// INTEGRITY STATEMENT (v4)
+//
+// By signing your name and ID below you are stating that you have agreed
+// to the online academic integrity statement posted on edX:
+// (Course > Assignments > Assignment Information & Policies > Academic Integrity Policy)
+/////////////////////////////////////////////////////////////////////////////////////////
+// I received help from and/or collaborated with:
+
+// ERROR_NO_INTEGRITY_STATEMENT
+//
+// Name: ERROR_NO_NAME
+// login ID: ERROR_NO_LOGIN
+//////////////////////////////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
